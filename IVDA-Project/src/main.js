147e85ecca186cfd861411a074d9842940475c0d
nanashuka <164695704@qq.com>
2022-12-06 23:04:14 +0100

    webpack

---
+++ IVDA-Project/src/main.js
@@ -0,0 +1,36 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/en'
+import App from './App'
+// import './registerServiceWorker'
+import router from './router'
+import store from './store'
+import iFrameResize from 'iframe-resizer/js/iframeResizer'
+
+// collapse 展开折叠
+import CollapseTransition from 'element-ui/lib/transitions/collapse-transition';
+
+Vue.use(ElementUI, { locale })
+Vue.use(VueAxios, axios);
+
+Vue.config.productionTip = false
+
+Vue.directive('resize', {
+  bind: function(el, { value = {} }) {
+    el.addEventListener('load', () => iFrameResize(value, el))
+  }
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})

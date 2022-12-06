9536ad5d8bef1a0549e15fca2d51deb30f7e4ee2
nanashuka <164695704@qq.com>
2022-12-06 17:55:03 +0100

    before data

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/en'
@@ -13,6 +15,7 @@ import CollapseTransition from 'element-ui/lib/transitions/collapse-transition';
 
 
 Vue.use(ElementUI, { locale })
+Vue.use(VueAxios, axios);
 
 Vue.config.productionTip = false
 
